a24333e6cf702e702972b91d4370d98cae185a5e
Foximeant <dimayablochkin14@gmail.com>
2026-05-03 00:35:46 +0300

    Update core.lua

--- core.lua
+++ core.lua
@@ -6,7 +6,7 @@ local auction = EPBossAuction
 -- Настройки и переменные
 -- ======================
 auction.prefix = "EPBAUC"
-auction.version = "2.0.19"
+auction.version = "2.1.9"
 auction.debug = true
 auction.fullyLoaded = false
 auction.pendingWorldEnter = nil
